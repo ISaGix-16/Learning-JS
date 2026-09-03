37f24095af2f0fd0678054bb26a8cb08f162ec0e
Aman Arya <itsaman24916@gmail.com>
2026-09-03 07:18:54 +0000

    rename

---
+++ basics/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS cod e as neweer version
+
+// alert("hello") // works in chrome console not in node js
+
+let name = "Isagi" // string
+let age = 19 // number
+let isLoggedIn = true // boolean
+
+// number
+// bigint
+// string
+// boolean => true / false
+// null => standalone value 
+// undefined => value not assiggned
+// symbol => unique
+
+
+// object
+
+console.log(typeof "isagi");
