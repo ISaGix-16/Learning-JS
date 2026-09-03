034bc86bb362d44d0e08ea93b2165774e8f3f6c3
IsAGi-auRa <itsaman24916@gmail.com>
2026-09-03 14:28:10 +0530

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,32 @@
+// Primitive 
+
+// 7 types : String, Number, Boolean, null, Undefined, Symbol, bigint
+
+// JavaScript is a dynamically typed language.
+
+// Symbol
+const id = Symbol('09')
+const secondId = Symbol('09')
+
+console.log(id === secondId);
+
+const bigNumber = 948904803604936409633454n // bigint
+
+// Reference Type (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["IronMan", "Spider Man"] // array
+
+let myObl = {
+    name: "Isagi",
+    age: 19,
+}           // Object
+
+const myFunc = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+
+
