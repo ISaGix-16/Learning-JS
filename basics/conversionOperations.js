d605c95d2e3d394d13433a3fb7d19fc8fd41dc8f
IsAGi-auRa <itsaman24916@gmail.com>
2026-09-03 13:37:21 +0530

    Operations Done

--- basics/conversionOperations.js
+++ basics/conversionOperations.js
@@ -25,4 +25,28 @@ let stringNumber = String(someNumber) */
 
 let value = 3
 let negValue = -value
-console.log(negValue);
+// console.log(negValue);
+
+// (x ** y) x - base, y - power
+
+let str1 = "hello"
+let str2 = " Isagi"
+
+let str3 = str1 + str2
+/* console.log(str3);
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+console.log(+true); // 1
+console.log(+""); // 0 */
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+
+console.log(gameCounter);
